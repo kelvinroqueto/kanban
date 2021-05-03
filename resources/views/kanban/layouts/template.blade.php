a862130e51ea444eb1282dab4f17fc371e1222f7
kelvinroqueto <kelvin_roqueto@hotmail.com>
2021-05-03 07:21:33 -0300

    update template

--- resources/views/kanban/layouts/template.blade.php
+++ resources/views/kanban/layouts/template.blade.php
@@ -37,11 +37,11 @@
         <script src="{{ asset("assets/bootstrap-3.3.7/js/bootstrap.min.js") }}"></script>
 
         <!-- selectpicker -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
 
-<!-- bootbox -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"></script>
+        <!-- bootbox -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"></script>
 
 
 
